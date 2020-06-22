4ddce8e0e16e5176168d77bdd3253e074b413921
saraMaleki <41663799+saraMaleki@users.noreply.github.com>
2020-06-22 10:25:21 +0430

    'incomeAndSomeBeauty'

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+
 
 # Create your models here.
+
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # return self.text
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # return self.text
+        return "{}-{}".format(self.date, self.amount)
+
+
